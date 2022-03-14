c4d297585173f7765d250700b16732dea73b00a6
Michael Callahan <mcallahan0@yahoo.com>
2022-03-13 22:03:52 -0400

    Added logic for calculating amount to pay for each person splitting the bill. Starting to set up segue to ResultsVC.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -14,6 +14,8 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
+    
+    var tip = 0.10
 
     @IBAction func tipChanged(_ sender: UIButton) {
         
@@ -22,14 +24,40 @@ class CalculatorViewController: UIViewController {
         twentyPctButton.isSelected = false
         sender.isSelected = true
         
+        let buttonTitle = sender.currentTitle!
+        
+        let buttonTitleNoPercent = String(buttonTitle.dropLast())
+        
+        let buttonTitleAsNumber = Double(buttonTitleNoPercent)!
+        
+        tip = buttonTitleAsNumber / 100
+        
+        billTextField.endEditing(true)
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         
+        let billAmount = billTextField.text!
+        if billAmount != "" {
+            let tipBillAmount = Double(billAmount)! + Double(billAmount)! * tip
+            let splitAmount = tipBillAmount / Double(splitNumberLabel.text!)!
+            let roundedSplitAmount = round(splitAmount * 100) / 100
+            print(roundedSplitAmount)
+        }
+        
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            
+        }
     }
     
 }
