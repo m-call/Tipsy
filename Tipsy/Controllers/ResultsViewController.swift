1d509311767c080aa9c0a016e85c99cdf1ae6fe2
Michael Callahan <mcallahan0@yahoo.com>
2022-03-13 22:44:24 -0400

    Created variables to display in ResultsVC. Set values in ResultsViewController for when the segue happens.

--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -8,21 +8,23 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
     var result = "0.0"
-    var tip = 0
-    var split = 0
+    var tip = 10
+    var split = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
 
